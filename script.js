303a69e9fdc69ee22c50b8a55f80e72b70cc648e
emukhaevvv <emukhaevvv@yandex.ru>
2024-04-01 16:55:07 +0300

    delete, void, ??

---
+++ script.js
@@ -0,0 +1,37 @@
+/*
+!   Eval
+
+?   Change global variable from eval
+
+let x = 50;
+eval("x = 90");
+
+?   Adding variable to global 
+?   working only with var and w/o variable;
+
+eval("var y = 50");
+*/
+
+/*
+! ??
+? First null or undefined
+
+let x = 5;
+let y = null;
+let z = x ?? y;
+*/
+
+/*
+! delete
+
+let x = 0;
+delete x; // false, use strict = error
+delete 1; // true
+*/
+
+/*
+! void
+
+let counter = 0;
+const count = () => void counter++; // undefined
+*/
