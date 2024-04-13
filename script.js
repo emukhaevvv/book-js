4dd62d9b9be1b48f6f380f0f6c6e04851c976440
emukhaevvv <emukhaevvv@yandex.ru>
2024-04-13 11:45:49 +0300

    array

---
+++ script.js
@@ -0,0 +1,20 @@
+// const x = Array.of(2);
+// const x = Array.from("1234", (v) => v + "2");
+
+/*
+! Length
+const a = [1, 2, 3, 4, 5];
+a.length = 2;
+*/
+
+/*
+const array = [1, 2, undefined, undefined, 34];
+
+const obj = {
+  name: "peter",
+};
+
+array.forEach(function (item, index, arr) {
+  console.log(this);
+}, obj);
+*/
