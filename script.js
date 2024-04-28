37afe50b0e5d76014284b34a166d7d2ae52fefba
emukhaevvv <emukhaevvv@yandex.ru>
2024-04-28 13:43:23 +0300

    Методы

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ array.forEach(function (item, index, arr) {
   console.log(this);
 }, obj);
 */
+
+/*
+const arr = [4, 2, 7, [9], [1], 0];
+
+const flatted = arr.flat();
+const flattMapped = arr.flatMap((x) => x + ": number");
+
+const spliced = [1, 2, 3, 4, 5];
+spliced.splice(2, 1); // 1,2,4,5;
+
+const sliced = spliced.slice(0, 1);
+spliced.fill(9, 1, 3 );
+*/
+
+/*
+const arr = ["ПН", "ВТ", "СР", "ЧТ", "ПТ"];
+arr.copyWithin(3, 0, 2);
+*/
+
+/*
+const arr = [5, 4, 8, 2, 0, 1];
+arr.sort((a, b) => {
+  if (a < b) return -1;
+  if (a > b) return 1;
+  return 0;
+});
+*/
