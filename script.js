e9dded883eac1a5abe9d7e374c2c7bdb8cc02fd7
emukhaevvv <emukhaevvv@yandex.ru>
2024-04-29 13:53:17 +0300

    functions

---
+++ script.js
@@ -0,0 +1,23 @@
+//! This
+/*
+const o = {
+  name: "vova",
+  m() {
+    const test = 10;
+    const t = () => {
+      console.log(this);
+    };
+    t();
+  },
+};
+*/
+
+//! Конструктор
+/*
+function User(name, age) {
+  this.name = name;
+  this.age = age;
+
+  return 5; // Ignore
+}
+*/
