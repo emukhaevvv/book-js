7157e71fe9c0797f8bf1c9fec66eb15a30d60aca
emukhaevvv <emukhaevvv@yandex.ru>
2024-06-16 17:01:51 +0300

    Добить

--- API/console/index.js
+++ API/console/index.js
@@ -37,3 +37,15 @@ console.group("1", [1, 2, 3]);
 console.time("Четыре");
 console.timeLog("Четыре");
 */
+
+/*
+console.log(
+    "Привет %s, я число: %d, я плаваю %f, я объект %O, я css: %c",
+    "Vovka",
+    20,
+    20.23232,
+    { name: "eg", age: 49 },
+    "color: red"
+  );
+  
+*/
